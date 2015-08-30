f89ee1d77448935efeab415fc1a08ae740f3885d
D. Mason <dmason@pitaro.(none)>
2015-08-29 21:46:20 -0500

    slight modification of the standard Blink example

---
+++ Blink/Blink.ino
@@ -0,0 +1,31 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+  DAM: Additional loop to count the delay down  
+  This example code is in the public domain.
+ */
+ 
+// Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+int led = 13;
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);     
+}
+
+int j;
+
+// the loop routine runs over and over again forever:
+
+// But can't just do the simple blinking bit...  Sort of count down now
+
+void loop() {
+  for (j=1000;j>0;j-=100) {
+    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(j);               // wait for a j/1000th of a second
+    digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+    delay(j);               // wait for a j/1000th of a second
+  }
+}
